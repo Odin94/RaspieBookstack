eb404cd6d396abd450aed64ac28359d7ba1b4729
Odin <kammerloher.andreas@gmail.com>
2019-04-07 20:04:53 +0200

    Move templates to separate folder, build templates to semantically named target folders

--- insert_variables.sh
+++ insert_variables.sh
@@ -6,16 +6,20 @@ echo "Replacing variables in docker-compose_template.yaml and backup_template.sh
 dir=`dirname $0`
 source "$dir/variables.sh"
 
-mkdir -p "actual"
+scripts_dir="backup_scripts"
+mkdir -p $scripts_dir
 
-echo "Will write output to $dir/actual"
+bookstack_dir="bookstack"
+mkdir -p $bookstack_dir
+
+echo "Will write output to $dir/$scripts_dir and $dir/$bookstack_dir"
 
 
-sed "s/__root_password__/${mysql_root_password}/g; s/__user_password__/${user_password}/g;" $dir/docker-compose_template.yaml > $dir/actual/docker-compose.yaml
-sed "s/__root_password__/${mysql_root_password}/g;" $dir/backup_template.sh > $dir/actual/backup.sh
-sed "s/__root_password__/${mysql_root_password}/g;" $dir/restore_template.sh > $dir/actual/restore.sh
+sed "s/__root_password__/${mysql_root_password}/g; s/__user_password__/${user_password}/g;" $dir/docker-compose_template.yaml > $dir/$bookstack_dir/docker-compose.yaml
+sed "s/__root_password__/${mysql_root_password}/g;" $dir/backup_template.sh > $dir/$scripts_dir/backup.sh
+sed "s/__root_password__/${mysql_root_password}/g;" $dir/restore_template.sh > $dir/$scripts_dir/restore.sh
 
-chmod +x $dir/actual/backup.sh
-chmod +x $dir/actual/restore.sh
+chmod +x $dir/$scripts_dir/backup.sh
+chmod +x $dir/$scripts_dir/restore.sh
 
-echo "done."
+echo "done. You can now run docker-compose up in \'$bookstack_dir\'"
