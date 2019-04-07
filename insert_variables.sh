a18f8602bea086e9b27bb66d3d30cbdf0121efd6
Andreas Kammerloher <kammerloher.andreas@gmail.com>
2019-04-07 13:54:19 +0000

    Stop echoing password

--- insert_variables.sh
+++ insert_variables.sh
@@ -10,7 +10,6 @@ mkdir -p "actual"
 
 echo "Will write output to $dir/actual"
 
-echo $user_password
 
 sed "s/__root_password__/${mysql_root_password}/g; s/__user_password__/${user_password}/g;" $dir/docker-compose_template.yaml > $dir/actual/docker-compose.yaml
 sed "s/__root_password__/${mysql_root_password}/g;" $dir/backup_template.sh > $dir/actual/backup.sh
